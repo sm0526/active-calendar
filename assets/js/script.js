b4c42f2f66311204a1333f249a91aab485a37ad9
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-07-26 22:11:46 -0600

    added in start variables and function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 //Need to create Variables
   //Need one for the hours
+  var hour = 7
   //Need one for the minutes
-  //Need one for the text input
+  var minutes = ":00"
+  //Need one for the text input - in functions instead of global maybe?
+  
 
 //Need to create Functions
   //Need a way to input text on certain hours
+  textInputEL = () => {
+    textInput = "";
+  };
   //Need a way to change the text input
   
 //Need to create Special Functions

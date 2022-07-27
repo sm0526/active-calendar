afa7d019d39b7940abd2641b65ea5e5dad9efc5a
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-07-26 21:57:19 -0600

    added javascript to base code

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Need to create Variables
+  //Need one for the hours
+  //Need one for the minutes
+  //Need one for the text input
+
+//Need to create Functions
+  //Need a way to input text on certain hours
+  //Need a way to change the text input
+  
+//Need to create Special Functions
+  //Need a way to save the user input to the server
+
+//+/- Logic

e9c83858b7ff691811f915c89a85e1b0cd2f4853
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-06 17:56:33 -0700

    removed all current js, aside from the commented to-do

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,11 @@
 //Need to create Variables
-  //Need one for the hours
-  var hour = 7
-  //Need one for the minutes
-  var minutes = ":00"
-  //Need one for the text input - in functions instead of global maybe?
-  
 
 //Need to create Functions
   //Need a way to input text on certain hours
-  textInputEL = () => {
-    textInput = "";
-  };
+ 
   //Need a way to change the text input
   
 //Need to create Special Functions
-  //Need a way to save the user input to the server
+
 
 //+/- Logic
